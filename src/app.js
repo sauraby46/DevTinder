2ab0000801492579bfc87116eaa969c20cbf9dfa
Saurab Yadav <saurabyadav0485@gmail.com>
2026-02-10 12:52:48 +0530

    built chat feature using web socket

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const connectDB = require("./config/database.js");
 const cookieParser = require('cookie-parser');
 const app = express();
+const http = require("http");
+const initializeSocket = require("./utils/socket");
 
 
 app.use(express.json());
@@ -15,25 +17,31 @@ const requestRouter = require("./routes/requests.route.js");
 const userRouter = require("./routes/user.route.js");
 const paymentRouter = require("./routes/payment");
 const cors = require('cors');
+const chatRouter = require('./routes/chat.js');
 
-app.use(cors(
+app.use(
+    cors(
     {
         origin: 'http://localhost:5173',
         credentials: true,
     }
 ));
+
+
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 app.use("/", paymentRouter);
+app.use("/", chatRouter);
 
-
+const server = http.createServer(app);
+initializeSocket(server);
 
 connectDB()
     .then(() => {
 
-      app.listen(process.env.PORT, () => {
+      server.listen(process.env.PORT, () => {
         console.log(`Server is running on port ${process.env.PORT}`);
 
     });
